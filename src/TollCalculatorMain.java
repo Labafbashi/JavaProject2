994734a7bdb1d1a426b3d9f8125a15c5e3d5eb22
hadi-tghl <hadi@gmx.se>
2021-12-17 14:09:02 +0100

    Add comments to main class

--- src/TollCalculatorMain.java
+++ src/TollCalculatorMain.java
@@ -4,10 +4,19 @@ import java.util.Date;
 
 public class TollCalculatorMain {
     public static void main(String[] args) {
+        // Create car with some car details
+        // Can be change for any car details, exactly as shown below
         Car car1 = new Car("C-Klass",2012,"Mercedes-Benz","SVG 312");
+
+        // Create motorbike with some input
+        // can be change for any motorbike details, exactly as show below
         MotorBike mb1 = new MotorBike("Seventy-Two", 2020,"HarleyDavidson","DFL 982");
+
+        // Calling Toll Calculator class
         TollCalculator tc = new TollCalculator();
 
+        // Making date as a string
+        // converting to simple date format
         String sDate1="2021/12/11 09:10:23";
         String sDate2="2021/12/11 09:11:10";
         Date date1= null;
